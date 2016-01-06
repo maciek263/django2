d410cf1af7506c3d1f2bf2ea0399116524717919
Maciej Jurkowski <maciek263@yahoo.pl>
2016-01-06 23:44:52 +0100

    rozszerzenia: publikacja, usuwanie

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
@@ -18,6 +18,7 @@ def post_new(request):
 		if form.is_valid():
 			post = form.save(commit=False)
 			post.author = request.user
+			#post.published_date = timezone.now()	#publikowanie tekstu odrazu
 			post.save()
 			return redirect('blog.views.post_detail', pk=post.pk)
 	else:
@@ -35,3 +36,32 @@ def post_edit(request, pk):
 	else:
 		form = PostForm(instance=post)
 	return render(request, 'blog/post_edit.html', {'form': form})
+	
+def post_draft_list(request):
+		posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+		return render(request, 'blog/post_draft_list.html', {'posts': posts})
+		
+def post_publish(request, pk):
+		post = get_object_or_404(Post, pk=pk)
+		post.publish()
+		return redirect('blog.views.post_detail', pk=pk)
+
+def post_remove(request, pk):
+		post = get_object_or_404(Post, pk=pk)
+		post.delete()
+		return redirect('blog.views.post_list')
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
