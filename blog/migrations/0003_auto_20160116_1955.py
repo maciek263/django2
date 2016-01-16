d9cdc49197b4d15ce6b44542c23b47362bad54ff
Maciej Jurkowski <maciek263@yahoo.pl>
2016-01-16 19:59:36 +0100

    komentarze, kolory

---
+++ blog/migrations/0003_auto_20160116_1955.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0002_post_approved_comment'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Comment',
+            fields=[
+                ('id', models.AutoField(primary_key=True, auto_created=True, verbose_name='ID', serialize=False)),
+                ('author', models.CharField(max_length=200)),
+                ('text', models.TextField()),
+                ('created_date', models.DateTimeField(default=django.utils.timezone.now)),
+                ('approved_comment', models.BooleanField(default=False)),
+            ],
+        ),
+        migrations.RemoveField(
+            model_name='post',
+            name='approved_comment',
+        ),
+        migrations.AddField(
+            model_name='comment',
+            name='post',
+            field=models.ForeignKey(to='blog.Post', related_name='comments'),
+        ),
+    ]
--- blog/models.py
+++ blog/models.py
@@ -10,12 +10,27 @@ class Post(models.Model):
 				default=timezone.now)
 	published_date = models.DateTimeField(
 				blank = True, null = True)
-				
+
 	def publish(self):
 		self.published_date = timezone.now()
 		self.save()
-		
-	
+
+
 	def __str__(self):
 		return self.title
 
+class Comment(models.Model):
+	post = models.ForeignKey('blog.Post', related_name='comments')
+	author = models.CharField(max_length=200)
+	title = models.TextField
+	text = models.TextField()
+	created_date = models.DateTimeField(default=timezone.now)
+	approved_comment = models.BooleanField(default=False)
+
+	def publish(self):
+		self.approved_comment = True
+		self.save()
+
+
+	def __str__(self):
+		return self.title
