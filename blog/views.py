801f0d0320d4fbb2e5df7c82a6f1ef3ad3d9d481
Maciej Jurkowski <maciek263@yahoo.pl>
2016-02-04 22:55:27 +0100

    poprawki, i dalsza walka z komentarzami

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm #, CommentForm #po odchaczeniu sypie sie cala strona
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html',{'posts': posts})
-	
+
 def post_detail(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	return render(request, 'blog/post_detail.html', {'post': post})
 
-@login_required	
+@login_required
 def post_new(request):
 	if request.method == "POST":
 		form = PostForm(request.POST)
@@ -26,11 +26,12 @@ def post_new(request):
 	else:
 		form = PostForm()
 	return render(request, 'blog/post_edit.html', {'form': form})
-	
+
 @login_required
 def post_edit(request, pk):
 	post = get_object_or_404(Post, pk=pk)
 	if request.method =="POST":
+		form = PostForm(request.POST, instance=post)
 		if form.is_valid():
 			post = form.save(commit=False)
 			post.author = request.user
@@ -40,34 +41,32 @@ def post_edit(request, pk):
 		form = PostForm(instance=post)
 	return render(request, 'blog/post_edit.html', {'form': form})
 
-@login_required	
+@login_required
 def post_draft_list(request):
 		posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
 		return render(request, 'blog/post_draft_list.html', {'posts': posts})
-		
-@login_required		
+
+@login_required
 def post_publish(request, pk):
 		post = get_object_or_404(Post, pk=pk)
 		post.publish()
 		return redirect('blog.views.post_detail', pk=pk)
-		
+
 @login_required
 def post_remove(request, pk):
 		post = get_object_or_404(Post, pk=pk)
 		post.delete()
 		return redirect('blog.views.post_list')
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+def add_comment_to_post(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	if request.method == "POST":
+		form = CommentForm(request.POST)
+		if form.is_vaild():
+			comment = form.save(commit=False)
+			comment.post = post
+			comment.save()
+			return redirect('blog.views.post_detail', pk=post.pk)
+		else:
+			form = CommentForm()
+		return render(request, 'blog/add_comment_to_post.html', {'form': form})
